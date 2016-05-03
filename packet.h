782b26fef9727c7f98edb433a9739c55cd3b1d44
Jason Yang <jasonyang96@ucla.edu>
2016-05-03 13:54:05 -0700

    initial design of packet

--- packet.h
+++ packet.h
@@ -1,11 +1,15 @@
 #ifndef PACKET_H
 #define PACKET_H
 
-class Packet {
+#include <string>
+#include <sstream>
+
+class Header {
 public:
-    Packet(bool seq, bool ack, bool fin, int seq_num, int ack_num, int cont_len)
+    Header() {};
+    Header(char seq, char ack, char fin, const std::string &seq_num, const std::string &ack_num, const std::string &cont_len)
     {
-        seq = m_seq;
+        seq = m_syn;
         ack = m_ack;
         fin = m_fin;
         m_seq_num = seq_num;
@@ -13,13 +17,41 @@ public:
         m_cont_len = cont_len;
     }
 
+    void encode(std::stringstream &ss)
+    {
+        ss << m_syn << m_ack << m_fin << m_seq_num << m_ack_num << m_cont_len;
+    }
+
 private:
-    bool m_seq;
-    bool m_ack;
-    bool m_fin;
-    int  m_seq_num;
-    int  m_ack_num;
-    int  m_cont_len;
+    char        m_syn;
+    char        m_ack;
+    char        m_fin;
+    std::string m_seq_num;
+    std::string m_ack_num;
+    std::string m_cont_len;
+};
+
+class Packet {
+public:
+    Packet(char seq, char ack, char fin, const std::string &seq_num, const std::string &ack_num, const std::string &cont_len, const std::string &data)
+    {
+        m_header = Header(seq, ack, fin, seq_num, ack_num, cont_len);
+        size_t len = size_t(stoll(cont_len));
+        m_data.resize(len);
+        m_data = std::string(data, 0, len);
+    }
+
+    std::string encode()
+    {
+        std::stringstream ss;
+        m_header.encode(ss);
+        ss << m_data;
+        return ss.str();
+    }
+
+private:
+    Header      m_header;
+    std::string m_data;
 };
 
 #endif
