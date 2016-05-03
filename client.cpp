51bce65500037f87c891138f89a3671ef82b45b9
Jason Yang <jasonyang96@ucla.edu>
2016-05-03 11:48:23 -0700

    basic UDP connection between client and server

--- client.cpp
+++ client.cpp
@@ -1,9 +1,91 @@
-#include <string>
-#include <thread>
-#include <iostream>
+#include <cstring> // for memset
+#include <iostream> // for cout
+#include <stdio.h> // for perror
+#include <string> // for string
+#include <sys/socket.h> // for socket
+#include <sys/types.h> // for types
+#include <netdb.h> // for getaddrinfo
 
-int main()
+using namespace std;
+
+void process_error(int status, const string &function);
+
+int main(int argc, char* argv[])
 {
-  std::cerr << "web client is not implemented yet" << std::endl;
-  // do your stuff here! or not if you don't want to.
+    if (argc != 3)
+    {
+        cout << "Usage: " << argv[0] << " SERVER-HOST-OR-IP PORT-NUMBER" << endl;
+        return 1;
+    }
+
+    struct addrinfo hints;
+    struct addrinfo *res;
+    int status;
+
+    // set up hints addrinfo
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    //set up socket calls
+    int sockfd;
+    status = getaddrinfo(argv[1], argv[2], &hints, &res);
+    if (status != 0)
+    {
+        cerr << "getaddrinfo error: " << gai_strerror(status) << endl;
+        return 1;
+    }
+
+    // find socket to connect to
+    int yes = 1;
+    auto i = res;
+    for (; i != NULL; i = i ->ai_next)
+    {
+        sockfd = socket(res->ai_family, res->ai_socktype, 0);
+        if (sockfd == -1)
+        {
+            continue;
+        }
+
+        status = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+        if (status == -1)
+        {
+            continue;
+        }
+
+        status = connect(sockfd, res->ai_addr, res->ai_addrlen);
+        if (status == -1)
+        {
+            continue;
+        }
+
+        break;
+    }
+    freeaddrinfo(res);
+
+    // check if reached end of linked list
+    // means could not find a socket to bind to
+    if (i == NULL)
+    {
+        perror("bind to a socket");
+        return 1;
+    }
+
+    // recv data from server
+    while (1)
+    {
+        string test = "hello";
+        status = send(sockfd, test.c_str(), test.size(), 0);
+        process_error(status, "send");
+    }
+}
+
+void process_error(int status, const string &function)
+{
+    if (status == -1)
+    {
+        perror(&function[0]);
+        return 1;
+    }
 }
--- server.cpp
+++ server.cpp
@@ -1,9 +1,98 @@
-#include <string>
-#include <thread>
-#include <iostream>
+#include <cstring> // for memset
+#include <iostream> // for cout
+#include <stdio.h> // for perror
+#include <string> // for string
+#include <sys/socket.h> // for socket
+#include <sys/types.h> // for types
+#include <netdb.h> // for getaddrinfo
 
-int main()
+using namespace std;
+
+void process_error(int status, const string &function);
+
+int main(int argc, char* argv[])
 {
-  std::cerr << "web server is not implemented yet" << std::endl;
-  // do your stuff here! or not if you don't want to.
+    if (argc != 3)
+    {
+        cout << "Usage: " << argv[0] << " PORT-NUMBER FILE-NAME" << endl;
+        return 1;
+    }
+
+    // TODO: TRY TO OPEN FILE TO SERVE IT
+
+    struct addrinfo hints;
+    struct addrinfo *res;
+    int status;
+
+    // set up hints addrinfo
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    //set up socket calls
+    int sockfd;
+    status = getaddrinfo(NULL, argv[1], &hints, &res);
+    if (status != 0)
+    {
+        cerr << "getaddrinfo error: " << gai_strerror(status) << endl;
+        return 1;
+    }
+
+    // find socket to bind to
+    int yes = 1;
+    auto i = res;
+    for (; i != NULL; i = i ->ai_next)
+    {
+        sockfd = socket(res->ai_family, res->ai_socktype, 0);
+        if (sockfd == -1) 
+        {
+            continue;
+        }
+
+        status = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+        if (status == -1)
+        {
+            continue;
+        }
+
+        status = bind(sockfd, res->ai_addr, res->ai_addrlen);
+        if (status == -1)
+        {
+            continue;
+        }
+
+        break;
+    }
+    freeaddrinfo(res);
+
+    // check if reached end of linked list
+    // means could not find a socket to bind to
+    if (i == NULL)
+    {
+        perror("bind to a socket");
+        return 1;
+    }
+
+    // accept packets
+    while (1)
+    {
+        size_t buf_pos = 0;
+        string data;
+        data.resize(512);
+        int n_bytes = recv(sockfd, &data[buf_pos], data.size() - buf_pos, 0);
+        process_error(n_bytes, "recv");
+        cout << data << endl;
+        data.clear();
+        data.resize(512);
+    }
+}
+
+void process_error(int status, const string &function)
+{
+    if (status == -1)
+    {
+        perror(&function[0]);
+        return 1;
+    }
 }
