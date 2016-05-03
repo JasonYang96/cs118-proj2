d7c63969f232f0d6777b0c7726a68402dd78a361
Jason Yang <jasonyang96@ucla.edu>
2016-05-03 11:48:03 -0700

    spacing

--- packet.h
+++ packet.h
@@ -3,23 +3,23 @@
 
 class Packet {
 public:
-	Packet(bool seq, bool ack, bool fin, int seq_num, int ack_num, int cont_len)
-	{
-		seq = m_seq;
-		ack = m_ack;
-		fin = m_fin;
-		m_seq_num = seq_num;
-		m_ack_num = ack_num;
-		m_cont_len = cont_len;
-	}
+    Packet(bool seq, bool ack, bool fin, int seq_num, int ack_num, int cont_len)
+    {
+        seq = m_seq;
+        ack = m_ack;
+        fin = m_fin;
+        m_seq_num = seq_num;
+        m_ack_num = ack_num;
+        m_cont_len = cont_len;
+    }
 
 private:
-	bool m_seq;
-	bool m_ack;
-	bool m_fin;
-	int  m_seq_num;
-	int  m_ack_num;
-	int  m_cont_len;
+    bool m_seq;
+    bool m_ack;
+    bool m_fin;
+    int  m_seq_num;
+    int  m_ack_num;
+    int  m_cont_len;
 };
 
 #endif
