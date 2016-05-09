6a2630fbf6eacdf14739483151764561e7a4e22e
Jason Yang <jasonyang96@ucla.edu>
2016-05-09 13:22:51 -0700

    got rid of header

--- packet.h
+++ packet.h
@@ -2,14 +2,13 @@
 #define PACKET_H
 
 #include <string>
-#include <sstream>
 #include <bitset>
 #include <cstring>
 
-class Header {
+class Packet {
 public:
-    Header() = default;
-    Header(bool syn, bool ack, bool fin, uint16_t seq_num, uint16_t ack_num, uint16_t cont_len)
+    Packet() = default;
+    Packet(bool seq, bool ack, bool fin, uint16_t seq_num, uint16_t ack_num, uint16_t cont_len, char *data)
     {
         m_flags[3] = 0;
         m_flags[2] = syn;
@@ -18,6 +17,7 @@ public:
         m_seq_num = seq_num;
         m_ack_num = ack_num;
         m_cont_len = cont_len;
+        strcpy(m_data, data);
     }
 
     bool syn_set()
@@ -50,59 +50,16 @@ public:
         return m_cont_len;
     }
 
-private:
-    std::bitset<4>   m_flags;
-    uint16_t         m_seq_num;
-    uint16_t         m_ack_num;
-    uint16_t         m_cont_len;
-};
-
-class Packet {
-public:
-    Packet() = default;
-    Packet(bool seq, bool ack, bool fin, uint16_t seq_num, uint16_t ack_num, uint16_t cont_len, char *data)
-    {
-        m_header = Header(seq, ack, fin, seq_num, ack_num, cont_len);
-        strcpy(m_data, data);
-    }
-
-    bool syn_set()
-    {
-        return m_header.syn_set();
-    }
-
-    bool ack_set()
-    {
-        return m_header.ack_set();
-    }
-
-    bool fin_set()
-    {
-        return m_header.fin_set();
-    }
-
-    uint16_t seq_num()
-    {
-        return m_header.seq_num();
-    }
-
-    uint16_t ack_num()
-    {
-        return m_header.ack_num();
-    }
-
-    uint16_t cont_len()
-    {
-        return m_header.cont_len();
-    }
-
     std::string data()
     {
         return m_data;
     }
 
 private:
-    Header m_header;
+    std::bitset<4>   m_flags; // one byte
+    uint16_t         m_seq_num; // 4 bytes
+    uint16_t         m_ack_num; // 4 bytes
+    uint16_t         m_cont_len; // 4 bytes
     char   m_data[512];
 };
 
