828eae0e4cdb0447e8cee78f06b49b028a13edd4
Jason Yang <jasonyang96@ucla.edu>
2016-05-24 13:38:55 -0700

    updated packet class for congestion control

--- packet.h
+++ packet.h
@@ -4,12 +4,16 @@
 #include <string>
 #include <bitset>
 #include <cstring>
+#include <ctime>
 #include <sys/time.h>
 
-const size_t DATA_LENGTH = 20;
+const size_t MSS = 20; // MAX IS 1032, CAN'T BE 1024 SINCE OUR HEADER IS MORE THAN 8 BYTES
+const size_t INITIAL_SSTHRESH = 1000;
+const size_t INITIAL_TIMEOUT = 500; // ms
 const uint16_t MSN = 30720;
 
-class Packet {
+class Packet
+{
 public:
     Packet() = default;
     Packet(bool syn, bool ack, bool fin, uint16_t seq_num, uint16_t ack_num, uint16_t data_len, const char *data)
@@ -64,7 +68,7 @@ private:
     uint16_t         m_seq_num;  // 4 bytes
     uint16_t         m_ack_num;  // 4 bytes
     uint16_t         m_data_len; // 4 bytes
-    char             m_data[DATA_LENGTH];
+    char             m_data[MSS];
 };
 
 
@@ -81,9 +85,15 @@ public:
     {
         return m_p;
     }
+
+    struct timeval get_time_sent()
+    {
+        return m_time_sent;
+    }
+
 private:
     Packet m_p;
-    timeval m_time_sent;
+    struct timeval m_time_sent;
 
 };
 #endif
