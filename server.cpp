c02200875e18b435470a2cae8897b0e9e9bf88ba
Jason Yang <jasonyang96@ucla.edu>
2016-05-30 17:35:07 -0700

    forgot to check if it compiled

--- server.cpp
+++ server.cpp
@@ -383,7 +383,7 @@ uint16_t update_window(const Packet &p, unordered_map<uint16_t, Packet_info> &wi
 
     while (base_num > p.ack_num())
     {
-        found = window.find(base_num);
+        auto found = window.find(base_num);
         if (found == window.end())
         {
             cerr << "could not find base_num packet with base_num " << base_num << " in window, update_window" << endl;
@@ -396,7 +396,7 @@ uint16_t update_window(const Packet &p, unordered_map<uint16_t, Packet_info> &wi
     }
     while (base_num < p.ack_num())
     {
-        found = window.find(base_num);
+        auto found = window.find(base_num);
         if (found == window.end())
         {
             cerr << "could not find base_num packet with base_num " << base_num << " in window, update_window" << endl;
